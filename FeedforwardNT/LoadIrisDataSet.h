23de7bc8f13beccf5fb97a6b46d7f5a52b27e0f9
Seddik hakime <seddikhakime@macpro.local>
2017-05-31 11:50:56 +0900

    First commit for a feedforward neural network implementation.

---
+++ FeedforwardNT/LoadIrisDataSet.h
@@ -0,0 +1,16 @@
+//
+//  LoadIrisDataSet.h
+//  FeedforwardNT
+//
+//  Created by Seddik hakime on 21/05/2017.
+//
+
+#ifndef LoadIrisDataSet_h
+#define LoadIrisDataSet_h
+
+#include <stdio.h>
+
+#endif /* LoadIrisDataSet_h */
+
+float * __nonnull * __nonnull loadIris(const char * __nonnull file, size_t * __nonnull len);
+float * __nullable * __nullable readFile(const char * __nonnull file, size_t * __nonnull len);
--- FeedforwardNT/main.c
+++ FeedforwardNT/main.c
@@ -3,13 +3,65 @@
 //  FeedforwardNT
 //
 //  Created by Seddik hakime on 21/05/2017.
-//  Copyright © 2017 ScienceSoul. All rights reserved.
 //
 
-#include <stdio.h>
+#include "Training.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    float **dataSet = NULL;
+    float **trainingData = NULL, **testData = NULL;
+    
+    char dataSetName[256];
+    char dataSetFile[256];
+    int epochs, miniBatchSize, ntLayers[100], dataDivisions[2], classifications[100], inoutSizes[2];
+    float eta, lambda;
+    
+    size_t len1=0, len2=0, tr1=0, tr2=0, ts1=0, ts2=0;
+    size_t numberOfLayers=0, numberOfDataDivisions=0, numberOfClassifications=0, numberOfInouts=0;
+    
+    if (argc < 2) {
+        fatal("FeedforwardNT", "missing argument for the input parameters file.");
+    }
+    
+    fprintf(stdout, "FeedforwardNT: start....\n");
+    
+    bool pthread = false;
+    if (argc == 3) {
+        if (strcmp(argv[2], "-pthread") == 0) {
+            pthread = true;
+        }
+    }
+    if (pthread) {
+        fprintf(stdout, "FeedforwardNT: multithreaded batch active.\n");
+    }
+    
+    memset(dataSetName, 0, sizeof(dataSetName));
+    memset(dataSetFile, 0, sizeof(dataSetFile));
+    
+    memset(ntLayers, 0, sizeof(ntLayers));
+    memset(dataDivisions, 0, sizeof(dataDivisions));
+    memset(classifications, 0, sizeof(classifications));
+    memset(inoutSizes, 0, sizeof(inoutSizes));
+    
+    fprintf(stdout, "FeedforwardNT: load input parameters:\n");
+    if (loadParameters(argv[1], dataSetName, dataSetFile, ntLayers, &numberOfLayers, dataDivisions, &numberOfDataDivisions, classifications, &numberOfClassifications, inoutSizes, &numberOfInouts, &epochs, &miniBatchSize, &eta, &lambda) != 0) {
+        fatal("FeedforwardNT", "failure reading input parameters.");
+    }
+    fprintf(stdout, "FeedforwardNT: done.\n");
+    
+    dataSet = loadData(dataSetName, dataSetFile, &len1, &len2);
+    
+    trainingData = createTrainigData(dataSet, 0, dataDivisions[0], &tr1, &tr2, classifications, numberOfClassifications, inoutSizes);
+    testData = createTestData(dataSet, len1, len2, dataDivisions[0], dataDivisions[1], &ts1, &ts2);
+    free_fmatrix(dataSet, 0, len1-1, 0, len2-1);
+    
+    fprintf(stdout, "FeedforwardNT: train neural network with the %s data set.\n", dataSetName);
+    SDG(trainingData, testData, tr1, tr2, ts1, ts2, ntLayers, numberOfLayers, inoutSizes, classifications, epochs, miniBatchSize, eta, lambda, pthread);
+    fprintf(stdout, "FeedforwardNT: all done.\n");
+    
+    free_fmatrix(trainingData, 0, tr1-1, 0, tr2-1);
+    free_fmatrix(testData, 0, ts1-1, 0, ts2-1);
+    
     return 0;
 }
